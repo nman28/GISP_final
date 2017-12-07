f23db0815ce9dc86515365ed807f561dc7f4cf5b
liaosteven <liaosteven51@gmail.com>
2017-12-06 20:24:24 -0500

    moved csv file to data folder

--- 03_logistic_regression.R
+++ 03_logistic_regression.R
@@ -5,25 +5,32 @@ library(dplyr)
 setwd("C:/Users/liaos/OneDrive/Documents/Brown/Senior Year/GISP_final")
 
 # In this section, we loop through each of the csv files, and read it in
-path = "./data/shots/"
-file.names <- dir(path, pattern =".csv")
+pathWarriors = "./data/shots/"
+pathRockets = "./data/HOU_shots/"
 
-listOfGames <- list()
+teamThrees <- function(path){
+  file.names <- dir(path, pattern =".csv")
+  listOfGames <- list()
 
-for (i in 1:length(file.names)) {
-  temp <- read.csv(paste0('data/shots/', file.names[i]))
+  for (i in 1:length(file.names)) {
+    temp <- read.csv(paste0('data/shots/', file.names[i]))
+    
+    listOfGames[[i]] <- temp
+  }
   
-  listOfGames[[i]] <- temp
+  
+  # Combine all the games into one data frame
+  teamShots <- bind_rows(listOfGames)
 }
 
-# Combine all the games into one data frame
-allShots <- bind_rows(listOfGames)
+warriors <- teamThrees(pathWarriors)
+rockets <- teamThrees(pathRockets)
+
 
 # Create the logistic regression with output variable shot_outcome
 shotMakeMod <<- glm(shot_outcome ~ distance + shooter_height + defender_height + shot_dist + def_angle + LeftOrRight
                   , data = allShots, family = "binomial")
-
 # test it on some test variables to see what the model gives.
-test <- data.frame(distance = 2, shooter_height = 190, defender_height = 190, 
-                   shot_dist = 60, def_angle = 88, LeftOrRight = 'Right')
+test <- data.frame(distance = 5, shooter_height = 170, defender_height = 170, 
+                   shot_dist = 23, def_angle = 20, LeftOrRight = 'Left')
 predict(shotMakeMod, newdata = test, type = "response")
