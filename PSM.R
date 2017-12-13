fa2f61d4e24bcbcbf8b311bc527048621d411045
Ozan Adiguzel <ozanadiguzel147@gmail.com>
2017-12-12 20:08:13 -0500

    PSM update

--- PSM.R
+++ PSM.R
@@ -1,6 +1,8 @@
-
 library(dplyr)
 library(MatchIt)
+
+library(png)
+library(plotrix)
 
 # For reproducibility
 set.seed(95)
@@ -42,22 +44,22 @@ str(steph$LeftOrRight)
 # note that some columns are collinear such as defence heigh and height difference. 
 # no need to include these variables
 fit_shot <- glm(shot_outcome ~ 
-             quarter + 
-             quarter_clock +
-             shot_clock +
-             shooter_X +
-             shooter_Y +
-             defender_X +
-             defender_Y +
-             player_distance +
-             defender_height +
-             shot_dist +
-             def_angle +
-             LeftOrRight +
-             def_reyting +
-             def_position, 
-           data = steph, 
-           family="binomial")
+                  quarter + 
+                  quarter_clock +
+                  shot_clock +
+                  shooter_X +
+                  shooter_Y +
+                  defender_X +
+                  defender_Y +
+                  player_distance +
+                  defender_height +
+                  shot_dist +
+                  def_angle +
+                  LeftOrRight +
+                  def_reyting,
+                #def_position, 
+                data = steph, 
+                family="binomial")
 
 # summary
 # right or left looks interesting
@@ -68,22 +70,23 @@ summary(fit_shot)
 # nearest method does not give very good matching, putting caliper decreases the number of matches
 # caliper method still gives a less but still significant p-value in paired t-test and mcnemar's test
 m.out <- matchit(LeftOrRight ~ 
-                    quarter + 
-                    quarter_clock +
-                    shot_clock +
-                    shooter_X +
-                    shooter_Y +
-                    defender_X +
-                    defender_Y +
-                    player_distance +
-                    defender_height +
-                    shot_dist +
-                    def_angle +
-                    def_reyting +
-                    def_position, 
-                  data = steph, 
-           #       method = "nearest",
-                 caliper = 0.3)
+                   quarter + 
+                   quarter_clock +
+                   shot_clock +
+                   shooter_X +
+                   shooter_Y +
+                   defender_X +
+                   defender_Y +
+                   player_distance +
+                   defender_height +
+                   shot_dist +
+                   def_angle +
+                   def_reyting,
+                 #def_position, 
+                 data = steph, 
+                 #       method = "nearest",
+                 caliper = 0.3
+)
 
 # summary
 summary(m.out)
@@ -107,7 +110,8 @@ yT		<- match_data$shot_outcome[group1]
 yC		<- match_data$shot_outcome[group2]
 
 # bind
-matched_cases	<- cbind(matches,	yT,	yC)
+matched_cases	<- cbind(matches,	group1, group2, yT,	yC)
+matched_cases <- na.omit(matched_cases)
 
 # create table for mcnemar's test
 tbl <- table(matched_cases$yT, matched_cases$yC)
@@ -123,5 +127,64 @@ t.test(matched_cases$yT,	matched_cases$yC,	paired	= TRUE, alternative = "two.sid
 # We can conduct paired t or mcnemar's test on every variables that we are matching for 
 # to see the final difference is significant or not
 
+# Add lines of players
+lines(kemba$x_loc, kemba$y_loc, type = 'b', col = '#00FFFF')
+lines(frank$x_loc, frank$y_loc, type = 'b', col = '#00BBFF')
+the.court <- readPNG('Basketball-Court.png')
+
+# Returns a graph given a row in matched_cases
+# mc is matched_cases
+# md is match_data
+# rightOrLeft is 1 for right (group 1), 0 for left (group 2)
+getGraph <- function(mc, md, row, rightOrLeft) {
+  
+  if (rightOrLeft) {
+    rowToPlot <- mc$group1[row]
+  } else {
+    rowToPlot <- mc$group2[row]
+  }
+  
+  data <- md[rowToPlot,]
+  
+  # We plot an empty graph (type = 'n') with dimensions 50x94
+  
+  
+  # Width and height are given in pixels
+  # Make sure to specify this!!!
+  #png('test.png', width = 250, height = 470)
+  
+  # windows(width = 5, height = 9.4)
+  plot(0:50, xlim=c(0,50), ylim=c(94, 0), type="n")
+  # par grabs the parameters from the current plot
+  lim <- par()
+  # rasterImage just slaps an image on top of the plot. It's is a dependency of the plotrix library
+  rasterImage(the.court, lim$usr[1], lim$usr[3], lim$usr[2], lim$usr[4])
+  
+  points(data$shooter_Y, data$shooter_X, type = "p", pch = 19, col = "red")
+  points(data$defender_Y, data$defender_X, type = "p", pch = 'x', col = "blue")
+  
+}
 
 
+# Returns:
+# Shooter name, Defender Name,
+# Defender Height, Defender Distance, Defender Angle, Defender Pass. Rating
+# MakeOrMiss
+
+# mc is matched_cases
+# md is match_data
+# rightOrLeft is 1 for right (group 1), 0 for left (group 2)
+getInfo <- function(mc, md, row, rightOrLeft) {
+  
+  if (rightOrLeft) {
+    rowToPlot <- mc$group1[row]
+  } else {
+    rowToPlot <- mc$group2[row]
+  }
+  
+  data <- md[rowToPlot,]
+  
+  ## data is the row that contains all the information. Format however 
+  ## you wish
+  
+}
